b55673b34a0c621f424c2cf6cd2120f4b217ab5d
Jacob <jckob21@gmail.com>
2022-05-30 15:50:23 +0200

    ADded single topic website

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Topics page
     path('topics/', views.topics, name='topics'),
+    # Single topic page
+    path('topic/<int:topic_id>/', views.topic, name='topic'),
 ]
