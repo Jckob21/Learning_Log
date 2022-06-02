49e66cfb2f5314973a60a484e1461dac2a2d4085
Jacob <jckob21@gmail.com>
2022-06-02 14:04:45 +0200

    Added user attribute to Topic model and restricted topics to view only the users' topics

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # To modify data used by learning log application:
 # 1. Modify models.py
@@ -12,6 +13,7 @@ class Topic(models.Model):
     """A topic user is learning about"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model"""
